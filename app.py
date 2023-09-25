721c921a09b60de928ee9372f6671efb2f884762
AzwadFawadHasan2020222 <azwadfawadhasan@gmail.com>
2023-09-25 12:40:32 +0600

    added dynamic data and api route

--- app.py
+++ app.py
@@ -1,15 +1,44 @@
 
 
 #module flask importing a class called Flask
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__) #creating a flask application and giving it a name
 
+
+JOBS = [
+    {
+        'id':1,
+        'title': 'Data Analyst',
+        'location': 'Dhaka, Bangladesh',
+        'salary': '100000'
+
+    },
+    {
+        'id': 2,
+        'title': 'Jr. Data Analyst',
+        'location': 'Dhaka, Bangladesh',
+        'salary': '10000'
+
+    },
+    {
+        'id': 3,
+        'title': 'Data Scientist',
+        'location': 'Dhaka, Bangladesh',
+       # 'salary': '144000'
+
+    }
+
+]
 #creating a route
 #@ is a decorator, provided by librarires to provide some advanced functionalities
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 """
 When the script is run, __name__ is set to "__main__" in that script.
 When the script is imported as a module into another script, __name__ is set to the script's name 
